d9932e7b654861ff0b171c887e315f404d1049a0
Inthiyas Shaik <159418828+shaikinthu@users.noreply.github.com>
2025-12-11 13:32:31 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,35 @@
+package com.example.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstname;
+	
+	@Column(name="last_name")
+	private String lastname;
+	
+	@Column(name="email")
+	private String email;
+
+	
+
+}
